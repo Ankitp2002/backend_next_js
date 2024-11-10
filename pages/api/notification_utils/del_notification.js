d228646ad768d85c967c61baa88ff22a80db8a9f
_Ankitdeveloper <ankit.prajapati@scriptalldna.com>
2024-11-10 14:55:27 +0530

    asd

--- pages/api/notification_utils/del_notification.js
+++ pages/api/notification_utils/del_notification.js
@@ -4,12 +4,17 @@ const prisma = new PrismaClient();
 
 export async function delNotification(req) {
   try {
-    const id = req?.query["id"];
-    return await prisma.user.delete({
-      where: { id: parseInt(id) },
+    const ids = req.body.ids;
+    const deletedUsers = await prisma.notificationAudit.deleteMany({
+      where: {
+        id: {
+          in: ids.map((id) => parseInt(id)), // Ensure IDs are integers
+        },
+      },
     });
+    return { message: "success" };
   } catch (error) {
-    throw new Error("Failed to del thesis");
+    throw new Error("Failed to del notification");
   } finally {
     await prisma.$disconnect();
   }
--- pages/api/notification_utils/get_notification.js
+++ pages/api/notification_utils/get_notification.js
@@ -1,25 +1,25 @@
 import { PrismaClient } from "@prisma/client";
-import { token_user_details } from "./create_notification";
+import { token_user_details } from "../user_utils/create_user";
 
 const prisma = new PrismaClient();
 
-async function get_user_by_role(req) {
-  return await prisma.user.findMany({
-    where: { role: req.query["role"] },
-  });
-}
 export async function getNotification(req) {
   try {
-    let user;
     const token = req?.headers["authorization"]?.split(" ")[1];
-    if (req?.query["role"]) {
-      user = get_user_by_role(req);
-    } else if (token) {
-      user = await token_user_details(token);
-    } else {
-      user = await prisma.notification.findMany({});
+    const user = await token_user_details(token);
+    if (user) {
+      const userNotification = await prisma.notificationAudit.findMany({
+        where: {
+          status: false, // Filter by status
+          userId: user.user_id, // Filter by userId
+        },
+        include: {
+          notification: true,
+        },
+      });
+      return userNotification;
     }
-    return user;
+    throw new Error("Invalid token");
   } catch (error) {
     throw new Error(error);
   } finally {
